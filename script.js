e5e95f83fb959283615bcabf01903e2c92ee85d2
Muhammad Abdullah <abdullahferoz313@gmail.com>
2024-06-17 13:14:04 +0500

    Added Extra top row of Buttons for AC Del Ans and a bottom 00 Button

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 console.log(`started javascript`);
 
 let displayValue = "";
+let answer = "0"
 let newInput = true;
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".button");
-
-document.addEventListener("keydown",(e)=>{
-    if(e.key = "Escape"){
-        displayValue = ""
-        display.querySelector("input").value=displayValue
-    }
-    
-})
 
 buttons.forEach((e)=>{
     e.addEventListener("click",()=>{
@@ -22,13 +15,26 @@ buttons.forEach((e)=>{
         if(e.value == "="){
             try{
                 displayValue = eval(displayValue);
+                answer = displayValue;
             }
             catch(error){
                 alert("Incorrect Syntax")
                 displayValue = ""
             }
             newInput = true;
-        }else{
+        }else if(e.value == "AC"){
+            displayValue = ""
+            display.querySelector("input").value=displayValue
+        }
+        else if(e.value == "Del"){
+            displayValue =  displayValue.slice(0,-1);
+            display.querySelector("input").value=displayValue
+        }
+        else if(e.value == "Ans"){
+            displayValue =  answer;
+            display.querySelector("input").value=displayValue
+        }
+        else{
             displayValue += e.value
         }
         display.querySelector("input").value=displayValue;
